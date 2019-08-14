54873186f095b2cbd956c5e89741fcd44bb0c6aa
shunfy <1175653149@qq.com>
2019-08-14 17:48:54 +0800

    AssetBundle.LoadFromStreamAsync使用

---
+++ Assets/Editor/AssetBuild.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public static class AssetBuild
+{
+    [MenuItem("Assets/打ab")]
+    static void build()
+    {
+        var obj = Selection.activeObject;
+        if (obj != null)
+        {
+            string path = AssetDatabase.GetAssetPath(obj);
+            string abName = path.Replace(Path.GetExtension(path), ".ab");
+            AssetBundleBuild build = new AssetBundleBuild();
+            build.assetBundleName = abName;
+            build.assetNames = new string[]{path};
+            BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath, new AssetBundleBuild[] { build }, BuildAssetBundleOptions.None, BuildTarget.Android);
+
+            StreamEncryption.EcryptAssetBundle(Application.streamingAssetsPath + "/" + abName);
+        }
+    }
+}
---
+++ Assets/test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StreamEncryption.DecryptAssetBundle(Application.streamingAssetsPath + "/assets/cube.ab", "Cube",(prefab)=>
+        {
+            var obj = GameObject.Instantiate<GameObject>(prefab as GameObject);
+            obj.transform.localPosition = new Vector3(0, 10, 0);
+        });
+
+        StartCoroutine(StreamEncryption.AsyncLoad(Application.streamingAssetsPath + "/assets/cube.ab", "Cube", (prefab) =>
+        {
+            var obj = GameObject.Instantiate<GameObject>(prefab as GameObject);
+            obj.transform.localPosition = new Vector3(0, -10, 0);
+        }));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
